88c21bd75c7cd5e5d5e5992ad281b46d66ecca16
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-12-29 10:30:24 +0200

    installed the react-icons package

--- src/context.js
+++ src/context.js
@@ -1,2 +1,4 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+// since we will wrap the whole app inside this provider, we need to add the {children}
