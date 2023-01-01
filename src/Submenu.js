cf5e4afca2d2e57a379b07f8720bf6a0f14441f4
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-01 19:48:35 +0200

    changed the number of columns accordingly

--- src/Submenu.js
+++ src/Submenu.js
@@ -7,14 +7,23 @@ const Submenu = () => {
     location,
     page: { page, links },
   } = useGlobalContext();
+
   const container = useRef(null);
+
+  const [columns, setColumns] = useState("col-2");
 
   useEffect(() => {
     const { center, bottom } = location;
     const submenu = container.current;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
-  }, [location]);
+
+    if (links.length === 3) {
+      setColumns("col-3");
+    } else if (links.length > 3) {
+      setColumns("col-4");
+    }
+  }, [location, links]);
 
   return (
     <aside
@@ -22,7 +31,7 @@ const Submenu = () => {
       ref={container}
     >
       <h4>{page}</h4>
-      <div className="submenu-center col-2">
+      <div className={`submenu-center ${columns}`}>
         {links.map((link, index) => {
           const { url, label, icon } = link;
           return (
